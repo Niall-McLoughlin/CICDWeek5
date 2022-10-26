142aab1a1d98e9ea56adfe6058c1a6c5b18fc607
G00362680 <G00362680@atu.ie>
2022-10-26 10:36:32 +0100

    tests completed

--- src/main/java/ie/atu/week5/Count.java
+++ src/main/java/ie/atu/week5/Count.java
@@ -1,4 +1,30 @@
 package ie.atu.week5;
 
 public class Count {
+    private int countValue;
+
+    public Count() {
+        countValue = 0;
+    }
+
+    public Count(int startingValue) {
+        if (startingValue > 5) {
+            countValue = startingValue;
+        }
+        else
+        {
+            throw new IllegalArgumentException("This is not a valid number");
+        }
+    }
+
+
+
+    public int increment() {
+        countValue++;
+        return countValue;
+    }
+    public int decrement() {
+        countValue--;
+        return countValue;
+    }
 }
